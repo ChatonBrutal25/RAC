5e2b4268559277b2bf71085d077c2104a251382d
ChatonBrutal25 <adrsup@outlook.fr>
2021-03-10 11:37:01 +0100

    commit modificationt

--- App.js
+++ App.js
@@ -7,25 +7,13 @@ import { TypePlugin } from './plugins/plugin-gestion.js';
 import {NavigationContainer} from '@react-navigation/native';
 import { NavigationView } from './components/navigation.js';
 
-const Drawer = new NavigationView(['VIEW 1' , 'VIEW 2']).render(); /* createDrawerNavigator();*/
-function DrawerContainer () {
-  return (
-  < Drawer/>/*.Navigator >
-  < Drawer.Screen name = "Contactez-nous" component = {ContactUs} />
-  < Drawer.Screen name = "Settings" component = {Settings} />
-  </ Drawer.Navigator >*/
-  );
-  }
 export default function App() {
 
   return (
-    /*<View style={styles.container}>
+    <View style={styles.container}>
       <Text>Miaou je suis un chat {TypePlugin.VISIBLE}</Text>
       <StatusBar style="auto" />
-    </View>*/
-    <NavigationContainer>
-      < DrawerContainer />
-    </NavigationContainer>
+    </View>
   );
 }
 const styles = StyleSheet.create({
@@ -39,17 +27,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
-function ContactUs() {
-  return (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-  <Text>Contact Us</Text>
-  </View>
-  );
-  }
-  function Settings() {
-  return (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-  <Text>Settings screen</Text>
-  </View>
-  );
-  }
